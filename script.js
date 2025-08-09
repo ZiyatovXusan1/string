6b335403e406d8b3cc17c3a4557e909338b79fb5
ZiyatovXusan1 <feruzgafurov502@gmail.com>
2025-08-09 15:35:59 +0500

    string

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// ! 1-masala
+// let n = 20;
+// let charCode = 65;
+// for( let i = 0; i <= n; i++ ) {
+//     console.log(String.fromCharCode(charCode + i));
+// }
+
+// ! 2-masala
+// let n = '1';
+// let a = n.charCodeAt(0);
+// if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) {
+//     console.log('lotin');
+// } else if (a >= 48 &&  a <= 57) {
+//     console.log('digit');
+// } else {
+//     console.log(0);
+// }
+
+// ! 3-masala
+// function matn(str) {
+//   if (!str || str.length === 0) {
+//     console.log("Belgilar topilmadi!!!");
+//     return;
+//   }
+
+//   const firstLetter = str.charCodeAt(0);
+
+//   const lastLetter = str.charCodeAt(str.length - 1);
+
+//   console.log("first letter", str[0], "=> ", firstLetter);
+//   console.log("last letter:", str[str.length - 1], "=> ", lastLetter);
+// }
+// matn('xusan')
+
+// ! 4-masala
+// function string (n, h) {
+//     if (typeof h !=='string' || h.length === 0) return '';
+
+//     if (n <= 0) return '';
+    
+//     return h[0].repeat(n);
+// };
+
+// console.log(string(4, 'x'));
